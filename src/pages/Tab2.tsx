7a7598a9d38f380b645f6e56cb6fe316b6d44b6e
JhnDm <jhunadomz@gmail.com>
2024-04-02 08:12:18 +0800

    Added the click counter in click counter tab

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,25 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
-const Tab2: React.FC = () => {
+
+  const ClickCounter: React.FC = () => {
+    const [count, setCount] = useState(0);
+  
+    const handleClick = () => {
+      setCount(count + 1);
+    };
+
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Click Counter</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </IonPage>
-  );
+    <IonContent>
+      <h2>Click Counter</h2>
+      <p>Number of clicks: {count}</p>
+      <IonButton onClick={handleClick}>Click me</IonButton>
+    </IonContent>
+  </IonPage>
+);
 };
 
-export default Tab2;
+export default ClickCounter;
