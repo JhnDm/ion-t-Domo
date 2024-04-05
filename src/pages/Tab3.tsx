8aa0ca0a143fc510cd4e6fc7e3e682eb129a7fdf
JhnDm <jhunadomz@gmail.com>
2024-04-05 08:25:25 +0800

    Added calculator code in tab3.tsx

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,8 +1,29 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
+import React, { useState } from 'react';
+
 
 const Tab3: React.FC = () => {
+  const [expression, setExpression] = useState('');
+
+  const handleClick = (value: string) => {
+    setExpression(prevExpression => prevExpression + value);
+  };
+
+  const calculateResult = () => {
+    try {
+      const result = eval(expression);
+      setExpression(result.toString());
+    } catch (error) {
+      setExpression('Error');
+    }
+  };
+
+  const clearExpression = () => {
+    setExpression('');
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -11,12 +32,32 @@ const Tab3: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+      <div className="calculator">
+      <input className="expression" value={expression} readOnly />
+      <div className="buttons">
+        <button className="button" onClick={() => handleClick('7')}>7</button>
+        <button className="button" onClick={() => handleClick('8')}>8</button>
+        <button className="button" onClick={() => handleClick('9')}>9</button>
+        <button className="button operator" onClick={() => handleClick('/')}>/</button>
+
+        <button className="button" onClick={() => handleClick('4')}>4</button>
+        <button className="button" onClick={() => handleClick('5')}>5</button>
+        <button className="button" onClick={() => handleClick('6')}>6</button>
+        <button className="button operator" onClick={() => handleClick('*')}>*</button>
+
+        <button className="button" onClick={() => handleClick('1')}>1</button>
+        <button className="button" onClick={() => handleClick('2')}>2</button>
+        <button className="button" onClick={() => handleClick('3')}>3</button>
+        <button className="button operator" onClick={() => handleClick('-')}>-</button>
+
+        <button className="button" onClick={() => handleClick('0')}>0</button>
+        <button className="button" onClick={() => handleClick('.')}>.</button>
+        <button className="button equal" onClick={calculateResult}>=</button>
+        <button className="button operator" onClick={() => handleClick('+')}>+</button>
+
+        <button className="button clear" onClick={clearExpression}>C</button>
+      </div>
+    </div>
       </IonContent>
     </IonPage>
   );
