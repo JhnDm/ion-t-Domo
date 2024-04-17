cdf3399158981ee9fec227395ed95830b357cdb9
JhnDm <jhunadomz@gmail.com>
2024-04-17 16:29:42 +0800

    added blanks in card

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,7 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonCard,IonCardCon
 import { useHistory } from 'react-router-dom'; // Import useHistory hook
 
 import './home.css';
-import { calculatorOutline, speedometerOutline } from 'ionicons/icons';
+import { calculatorOutline, closeOutline, speedometerOutline } from 'ionicons/icons';
 
 const Home: React.FC = () => {
   const history = useHistory(); // Initialize useHistory hook
@@ -34,8 +34,6 @@ const Home: React.FC = () => {
                 <IonLabel>Click Counter</IonLabel>
               </IonItem>
               <br />
-              <br />
-
               <IonItem>
                 <IonThumbnail slot="start">
                 <IonRouterLink href="./calculator">
@@ -44,6 +42,25 @@ const Home: React.FC = () => {
                 </IonThumbnail>
                 <IonLabel>Calculator</IonLabel>
               </IonItem>
+              <br />
+              <IonItem>
+                <IonThumbnail slot="start">
+                <IonRouterLink href="">
+                <IonIcon icon={closeOutline} style={{ fontSize: '4rem' }} />
+                    </IonRouterLink>
+                </IonThumbnail>
+                <IonLabel>Blank</IonLabel>
+              </IonItem>
+              <br />
+              <IonItem>
+                <IonThumbnail slot="start">
+                <IonRouterLink href="">
+                <IonIcon icon={closeOutline} style={{ fontSize: '4rem' }} />
+                    </IonRouterLink>
+                </IonThumbnail>
+                <IonLabel>Blank</IonLabel>
+              </IonItem>
+
 
             </IonList>
           </IonCardContent>
