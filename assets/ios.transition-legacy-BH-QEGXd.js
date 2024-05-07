c071d9293f1174a9bed92d7176443bdab7e9b2db
JhnDm <jhunadomz@gmail.com>
2024-05-07 09:17:32 +0800

    Updates

--- assets/ios.transition-legacy-BH-QEGXd.js
+++ assets/ios.transition-legacy-BH-QEGXd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-98r_ZRYb.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-DuFrQ7QE.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
